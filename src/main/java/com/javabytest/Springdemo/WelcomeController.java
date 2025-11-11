77076c09dadaa9b72a72649d28b9030eba1f74fa
Manish Puthran (PET-CP1-RD) <LC570251@calcomp.co.th>
2025-11-11 13:08:09 +0700

    Updated Spring Boot app with CORS fix

--- src/main/java/com/javabytest/Springdemo/WelcomeController.java
+++ src/main/java/com/javabytest/Springdemo/WelcomeController.java
@@ -1,25 +1,15 @@
 package com.javabytest.Springdemo;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*") // 👈 This allows frontend JS to call backend
 @RestController
-//@RequestMapping("/api/message")
-//@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class WelcomeController {
 
-	  @GetMapping("/api/message")
-	    public String getMessage() {
-	        return "Hello World From qwertysqi";
-	    }
+    @GetMapping("/api/message")
+    public String getMessage() {
+        return "Hello World From qwertysqi";
+    }
 }
-
-
-
-
